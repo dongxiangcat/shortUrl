a7857f085321f43fc58c3d9c5450c2dda56eed01
dongxiangcat <864340463@qq.com>
2021-11-01 22:31:32 +0800

    first commit

---
+++ src/main/java/pers/dongxiang/shorturl/dto/ShortUrlDTO.java
@@ -0,0 +1,23 @@
+package pers.dongxiang.shorturl.dto;
+
+import lombok.Data;
+
+/**
+ * @ProjectName short-url
+ * @Package pers.dongxiang.shorturl.dto
+ * @ClassName ShortUrlDTO
+ * @Description TODO
+ * @Company lab
+ * @Author dongxiang
+ * @Date 10/31/2021 1:21 PM
+ * @UpdateUser
+ * @UpdateDate
+ * @UpdateRemark
+ * @Version 1.0.0
+ */
+@Data
+public class ShortUrlDTO {
+
+    String shortUrl;
+
+}
